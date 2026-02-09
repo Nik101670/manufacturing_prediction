5f6a9a5729a8a46097b8192ef75e918253270fe5
Nik101670 <nikhilrambade16@gmail.com>
2026-02-09 11:32:13 +0530

    Added runtime.txt for Render

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://manufacturing-backend.onrender.com"
+
 
 st.set_page_config(page_title="Manufacturing App", layout="centered")
 
